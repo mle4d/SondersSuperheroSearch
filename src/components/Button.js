a480e557533bc934c0f92f36df3e35a906c93a0f
emily <mle4d@me.com>
2019-10-05 21:21:49 -0700

    file setup, buttonjs

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './button.css';
+
+
+
+
+function Button({ superheroRef, name, play, image }) {
+  return (
+    <>
+      <button className="hi" style={{ backgroundImage: `url(${image})` }} onClick={() => play(name)}></button>
+      <audio ref={superheroRef} />
+    </>
+  );
+}
+
+Button.propTypes = {
+  superheroRef: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  play: PropTypes.func.isRequired,
+  image: PropTypes.object.isRequired
+};
+
+export default Button;
