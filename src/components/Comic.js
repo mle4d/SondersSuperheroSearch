a4bc0075253bd6dbb3cb6a3457bbb3f1c25b7a5a
emily <mle4d@me.com>
2019-10-13 15:54:21 -0700

    got layout of page done with css

---
+++ src/components/Comic.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from './Button';
+import title from './assets/title.png';
+import Pow from './assets/Pow.png';
+import info from './assets/info.png';
+import styles from './comic.css';
+
+
+
+
+function Game() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.Title}><img src={title}></img></div>
+          <div className={styles.info}><img src={info}></img></div>
+        </div>
+        <div className={styles.comic}><img src={Pow}></img>
+          <Button />
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+export default Game;
