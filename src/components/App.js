6a783176b8b0b8c6bb1ed045720f59c4aca2b9a7
emily <mle4d@me.com>
2019-10-13 15:55:05 -0700

    starting layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import Game from './Comic';
+
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  
+  return (
+    <>
+      <Game/>
+    </>
+  );
 }
   
