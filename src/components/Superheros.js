27ed99c03a862901c221d472b908ad136719c1ea
emily <mle4d@me.com>
2019-10-16 21:51:52 -0700

    brought in more superheros

--- src/components/Superheros.js
+++ src/components/Superheros.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import shuffle from 'shuffle-array';
 import Button from './Button';
 import batmanrobinsound from './assets/sounds/batmanrobinsound.mp3';
 import batmansound from './assets/sounds/batmansound.mp3';
@@ -11,6 +10,13 @@ import spidermansound from './assets/sounds/spidermansound.mp3';
 import supermansound from './assets/sounds/supermansound.mp3';
 import transformersound from './assets/sounds/transformersound.mp3';
 import wolverinesound from './assets/sounds/wolverinesound.mp3';
+import buzzsound from './assets/sounds/buzzsound.mp3';
+import ironmansound from './assets/sounds/ironmansound.mp3';
+import wonderwomansound from './assets/sounds/wonderwomansound.mp3';
+import aquamansound from './assets/sounds/aquamansound.mp3';
+import blackpanthersound from './assets/sounds/blackpanthersound.mp3';
+import captainmarvelsound from './assets/sounds/captainmarvelsound.mp3';
+import elastigirlsound from './assets/sounds/elastigirlsound.wav';
 import batmanrobin from './assets/superheros/batmanrobin.png';
 import batman from './assets/superheros/batman.png';
 import captainamerica from './assets/superheros/captainamerica.png';
@@ -21,6 +27,13 @@ import spiderman from './assets/superheros/spiderman.png';
 import superman from './assets/superheros/superman.png';
 import bumblebee from './assets/superheros/bumblebee.png';
 import wolverine from './assets/superheros/wolverine.png';
+import buzz from './assets/superheros/buzz.png';
+import ironman from './assets/superheros/ironman.png';
+import wonderwoman from './assets/superheros/wonderwoman.png';
+import aquaman from './assets/superheros/aquaman.png';
+import blackpanther from './assets/superheros/blackpanther.png';
+import captainmarvel from './assets/superheros/captainmarvel.png';
+import elastigirl from './assets/superheros/elastigirl.png';
 import Pow from './assets/Pow.png';
 import styles from './button.css';
 
@@ -75,6 +88,41 @@ const superheroMap = {
     name: 'Wolverine',
     src: wolverinesound,
     image: wolverine
+  },
+  superhero11: {
+    name: 'Buzz',
+    src: buzzsound,
+    image: buzz
+  },
+  superhero12: {
+    name: 'Iron Man',
+    src: ironmansound,
+    image: ironman
+  },
+  superhero13: {
+    name: 'Wonder Woman',
+    src: wonderwomansound,
+    image: wonderwoman
+  },
+  superhero14: {
+    name: 'Aquaman',
+    src: aquamansound,
+    image: aquaman
+  },
+  superhero15: {
+    name: 'Black Panther',
+    src: blackpanthersound,
+    image: blackpanther
+  },
+  superhero16: {
+    name: 'Captain Marvel',
+    src: captainmarvelsound,
+    image: captainmarvel
+  },
+  superhero17: {
+    name: 'Elastigirl',
+    src: elastigirlsound,
+    image: elastigirl
   }
 };
 
@@ -90,11 +138,17 @@ export default class Superhero extends Component {
   superhero8 = React.createRef();
   superhero9 = React.createRef();
   superhero10 = React.createRef();
+  superhero11 = React.createRef();
+  superhero12 = React.createRef();
+  superhero13 = React.createRef();
+  superhero14 = React.createRef();
+  superhero15 = React.createRef();
+  superhero16 = React.createRef();
+  superhero17 = React.createRef();
 
   state = {
     playing: ''
   }
-  
   play = superheroId => {
     const ref = this[superheroId];
     this.setState({ playing: superheroMap[superheroId].name });
@@ -104,9 +158,10 @@ export default class Superhero extends Component {
   }
   
   render() {
-    const superheroArr = [this.superhero1, this.superhero2, this.superhero3, this.superhero4, this.superhero5, this.superhero6, this.superhero7, this.superhero8, this.superhero9, this.superhero10];
+    const superheroArr = [this.superhero1, this.superhero2, this.superhero3, this.superhero4, this.superhero5, this.superhero6, this.superhero7, this.superhero8, this.superhero9, this.superhero10, this.superhero11, this.superhero12, this.superhero13, this.superhero14, this.superhero15, this.superhero16, this.superhero17];
+    superheroArr.sort(() => 0.5 - Math.random());
     const buttonList = superheroArr.map((superhero, i) => {
-      return <Button image={superheroMap[`superhero${i + 1}`].image} key={i} superheroRef={superhero} name={`superhero${i + 1}`} play={this.play} />;
+      return <Button image={superheroMap[`superhero${i + 1}`].image} key={i} superheroRef={superhero} name={`superhero${i + 1}`} play={this.play} onClick={this.count}/>;
     });
     
     return (
